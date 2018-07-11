403eb7da42ebc95945b3ad37fc6707a348bbda4b
Marnie Reid <marniejeanreid@gmail.com>
2018-07-11 14:09:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-var newID = `${oldID} + 1000000000`
+var newID = 1000000000 + oldID
 return newID
